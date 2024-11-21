2a52c7349d58420d0928467b1bbdbf198e21d9b3
vinhlove9a <beochy204@gmail.com>
2024-11-21 11:41:09 +0700

    vinh commit

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+
+//click yes btn
+function clickYesButton() {
+    alert('Anh cung yeu Thu nhieu lam 8====>----()');
+}
+
+//move button on hover
+function hoverNoButton() {
+    // random from 0 to 1, then multiply with screen size
+    let x = Math.random() * window.innerWidth;
+    let y = Math.random() * window.innerHeight;
+
+    document.getElementById('no-button').style.left = x + 'px';
+    document.getElementById('no-button').style.top = y +'px';
+    
+}
